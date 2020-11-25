838080b93c9e308ad17daff40ff5e3efee814e51
Ben Hardill <hardillb@gmail.com>
2020-11-25 22:13:23 +0000

    Basic gophermap works for normal and Gopher+

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,7 @@ fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
 	let request = request.trim_end();
 
 
-	if request == "\t$" {
+	if request.ends_with("$") {
 		handle_plus(stream, request).unwrap();
 	} else {
 		handle_v1(stream, request).unwrap();
@@ -91,48 +91,73 @@ fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
 }
 
 fn handle_v1(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
-	let data = "iBen's Place - Gopher\tfake\t(NULL)\t0\r\n.\r\n";
 	let mut dir = env::current_dir().unwrap();
-	println!("CWD {}", dir.to_str().unwrap());
+	// println!("CWD {}", dir.to_str().unwrap());
 
-	// if request == "" {
-	// 	stream.write(&data.as_bytes()).expect("Failed to send");
-	// } else {
-		// let root = Path::new("./root");
-		dir.push("root");
-		// println!("root dir {}", dir.to_str().unwrap());
-		let request_path = Path::new(request);
+	dir.push("root");
+	// println!("root dir {}", dir.to_str().unwrap());
+	let request_path = Path::new(request);
+	if request.starts_with("/") {
 		let request_path = request_path.strip_prefix("/").unwrap();
 		dir.push(request_path);
-		let path = dir.as_path();
-		println!("requested path {}", path.to_str().unwrap());
+	}
+	let path = dir.as_path();
+	// println!("requested path {}", path.to_str().unwrap());
 
-		if path.exists() {
-			if path.is_dir() {
-				let map = path.join("gophermap");
-				println!("map path {}",map.to_str().unwrap());
-				if map.exists() {
-					let lines = read_gophermap(&map);
-					for l in lines {
-						stream.write(format!("{}{}\t{}\t{}\t{}\r\n", l.row_type, l.label, l.path, l.server, l.port).as_bytes()).expect("failed to send");
-					}
+	if path.exists() {
+		if path.is_dir() {
+			let map = path.join("gophermap");
+			// println!("map path {}",map.to_str().unwrap());
+			if map.exists() {
+				let lines = read_gophermap(&map);
+				for l in lines {
+					stream.write(format!("{}{}\t{}\t{}\t{}\r\n", l.row_type, l.label, l.path, l.server, l.port).as_bytes()).expect("failed to send");
 				}
-			} else {
-				stream.write(&request.as_bytes()).expect("Failed to send");
-			} 
+			}
 		} else {
-			let error = format!("3'{}' not found\r\n", request);
-			stream.write(&error.as_bytes()).expect("failed to send");
-		}
-	// }
+			stream.write(&request.as_bytes()).expect("Failed to send");
+		} 
+	} else {
+		let error = format!("3'{}' not found\r\n", request);
+		stream.write(&error.as_bytes()).expect("failed to send");
+	}
 
 	stream.flush().unwrap();
 	Ok(())
 }
 
 fn handle_plus(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
-	let data = "+-2\r\n+INFO: iBen's Place - Gopher\tfake\t(NULL)\t0\r\n+INFO: i\tfake\t(NULL)\t0\r\n+INFO: 0CV /cv.txt\tlocalhost\t70\t+\r\n";
-	stream.write(&data.as_bytes()).expect("Failed to send");
+	let mut dir = env::current_dir().unwrap();
+	dir.push("root");
+
+	let parts: Vec<&str> = request.split('\t').collect();
+	// println!("root dir {}", dir.to_str().unwrap());
+	let request_path = Path::new(parts[0]);
+	if parts[0].starts_with("/") {
+		let request_path = request_path.strip_prefix("/").unwrap();
+		dir.push(request_path);
+	}
+	let path = dir.as_path();
+
+	println!("requested path {}", path.to_str().unwrap());
+
+	if path.exists() {
+		if path.is_dir() {
+			let map = path.join("gophermap");
+			if map.exists() {
+				let lines = read_gophermap(&map);
+				stream.write("+-2".as_bytes()).expect("failed to send");
+				for l in lines {
+					stream.write(format!("+INFO: {}{}\t{}\t{}\t{}\r\n", l.row_type, l.label, l.path, l.server, l.port).as_bytes()).expect("failed to send");
+				}
+			}
+		} else {
+
+		}
+	}
+
+	//let data = "+-2\r\n+INFO: iBen's Place - Gopher\tfake\t(NULL)\t0\r\n+INFO: i\tfake\t(NULL)\t0\r\n+INFO: 0CV /cv.txt\tlocalhost\t70\t+\r\n";
+	//stream.write(&data.as_bytes()).expect("Failed to send");
 
 	stream.flush().unwrap();
 	Ok(())
