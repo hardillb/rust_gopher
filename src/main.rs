c6303b03ae5b647d888917e0a713817e33316d16
Ben Hardill <hardillb@gmail.com>
2020-11-26 22:07:08 +0000

    Add some cmd line config options

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // use std::fs;
+use std::io;
 use std::env;
 use std::fs::File;
 use std::io::{Write, BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
 use std::path::Path;
+
+use clap::{Arg, App};
 
 struct Gophermap {
 	row_type: char,
@@ -13,19 +16,68 @@ struct Gophermap {
 	port: i16
 }
 
-fn main() -> std::io::Result<()> {
-
-    println!("Hello, world!");
-	let listener = TcpListener::bind("0.0.0.0:70").unwrap();
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_connection(stream?).unwrap();
-    }
-    Ok(())
-
+struct Config {
+	hostname: String,
+	port: i16
 }
 
-fn read_gophermap(path: &Path) -> Vec<Gophermap>{
+fn main() -> std::io::Result<()> {
+
+	let matches = App::new("rust_gopher")
+		.version("0.1.0")
+		.author("Ben Hardill")
+		.about("Basic Gopher Server")
+		.arg(Arg::with_name("hostname")
+			.short("h")
+			.long("hostname")
+			.takes_value(true)
+			.help("The hostname of this server"))
+		.arg(Arg::with_name("port")
+			.short("p")
+			.long("port")
+			.takes_value(true)
+			.help("Port number to listen on"))
+		.get_matches();
+
+	let hostname = matches.value_of("hostname").unwrap_or("localhost");
+	let port :i16 = matches.value_of("port").unwrap_or("70").parse().unwrap();
+	println!("Listening on 0.0.0.0 and port {} as {}", port, hostname);
+
+	let bind_addr = format!("0.0.0.0:{}", port);
+
+	let config = Config {
+		hostname: hostname.to_string(),
+		port: port
+	};
+
+	let listener = TcpListener::bind(bind_addr).unwrap();
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_connection(stream?, &config).unwrap();
+    }
+    Ok(())
+}
+
+fn handle_connection(stream: TcpStream, config: &Config) -> std::io::Result<()> {
+	let mut request = String::new(); 
+
+	println!("Connection from {}", stream.peer_addr().unwrap());
+
+	let mut reader = BufReader::new(stream.try_clone().unwrap());
+	reader.read_line(&mut request).unwrap();
+	let request = request.trim_end();
+
+
+	if request.ends_with("$") {
+		handle_plus(stream, request, config).unwrap();
+	} else {
+		handle_v1(stream, request, config).unwrap();
+	}
+	Ok(())
+}
+
+
+fn read_gophermap(path: &Path, config: &Config) -> Vec<Gophermap>{
 	let mut entries: Vec<Gophermap> = Vec::new();
 
 	let file = File::open(path).unwrap();
@@ -37,24 +89,26 @@ fn read_gophermap(path: &Path) -> Vec<Gophermap>{
 		let mut label = l.to_string();
 		let mut p = "fake";
 		let mut s = "(NULL)";
+		let mut port = config.port;
 
 		if l.starts_with("0") {
  			t = '0';
  			l.remove(0);
  			let parts: Vec<&str> = l.split('\t').collect();
- 			println!("{:?}",parts);
+ 			// println!("{:?}",parts);
  			label = parts[0].to_string();
  			p = parts[1];
- 			s = "localhost";
+ 			
+ 			s = &config.hostname;
 
  		} else if l.starts_with("1") {
  			t = '1';
  			l.remove(0);
  			let parts: Vec<&str> = l.split('\t').collect();
- 			println!("{:?}",parts);
+ 			// println!("{:?}",parts);
  			label = parts[0].to_string();
  			p = parts[1];
- 			s = "localhost";
+ 			s = &config.hostname;
  		} 
  		// println!("line - {}", l);
  		let  entry = Gophermap {
@@ -62,7 +116,7 @@ fn read_gophermap(path: &Path) -> Vec<Gophermap>{
  			label: label,
  			path: p.to_string(),
  			server: s.to_string(),
- 			port: 70
+ 			port: port
  		};
 
  		entries.push(entry);
@@ -71,51 +125,31 @@ fn read_gophermap(path: &Path) -> Vec<Gophermap>{
 	entries
 }
 
-fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
-	let mut request = String::new(); 
-
-	println!("Connection from {}", stream.peer_addr().unwrap());
-
-	let mut reader = BufReader::new(stream.try_clone().unwrap());
-	reader.read_line(&mut request).unwrap();
-	let request = request.trim_end();
-
-
-	if request.ends_with("$") {
-		handle_plus(stream, request).unwrap();
-	} else {
-		handle_v1(stream, request).unwrap();
-	}
-
-	Ok(())
-}
-
-fn handle_v1(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
+fn handle_v1(mut stream: TcpStream, request: &str, config: &Config) -> std::io::Result<()> {
 	let mut dir = env::current_dir().unwrap();
 	// println!("CWD {}", dir.to_str().unwrap());
 
 	dir.push("root");
-	// println!("root dir {}", dir.to_str().unwrap());
-	let request_path = Path::new(request);
+	let mut request_path = Path::new(request);
 	if request.starts_with("/") {
-		let request_path = request_path.strip_prefix("/").unwrap();
-		dir.push(request_path);
+		request_path = request_path.strip_prefix("/").unwrap();
 	}
+	dir.push(request_path);
 	let path = dir.as_path();
-	// println!("requested path {}", path.to_str().unwrap());
 
 	if path.exists() {
 		if path.is_dir() {
 			let map = path.join("gophermap");
 			// println!("map path {}",map.to_str().unwrap());
 			if map.exists() {
-				let lines = read_gophermap(&map);
+				let lines = read_gophermap(&map, config);
 				for l in lines {
 					stream.write(format!("{}{}\t{}\t{}\t{}\r\n", l.row_type, l.label, l.path, l.server, l.port).as_bytes()).expect("failed to send");
 				}
 			}
 		} else {
-			stream.write(&request.as_bytes()).expect("Failed to send");
+			let mut file = File::open(path).unwrap();
+			io::copy(&mut file, &mut stream).unwrap();
 		} 
 	} else {
 		let error = format!("3'{}' not found\r\n", request);
@@ -126,7 +160,7 @@ fn handle_v1(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
 	Ok(())
 }
 
-fn handle_plus(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
+fn handle_plus(mut stream: TcpStream, request: &str, config: &Config) -> std::io::Result<()> {
 	let mut dir = env::current_dir().unwrap();
 	dir.push("root");
 
@@ -139,13 +173,11 @@ fn handle_plus(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
 	}
 	let path = dir.as_path();
 
-	println!("requested path {}", path.to_str().unwrap());
-
 	if path.exists() {
 		if path.is_dir() {
 			let map = path.join("gophermap");
 			if map.exists() {
-				let lines = read_gophermap(&map);
+				let lines = read_gophermap(&map, config);
 				stream.write("+-2".as_bytes()).expect("failed to send");
 				for l in lines {
 					stream.write(format!("+INFO: {}{}\t{}\t{}\t{}\r\n", l.row_type, l.label, l.path, l.server, l.port).as_bytes()).expect("failed to send");
@@ -155,9 +187,6 @@ fn handle_plus(mut stream: TcpStream, request: &str) -> std::io::Result<()> {
 
 		}
 	}
-
-	//let data = "+-2\r\n+INFO: iBen's Place - Gopher\tfake\t(NULL)\t0\r\n+INFO: i\tfake\t(NULL)\t0\r\n+INFO: 0CV /cv.txt\tlocalhost\t70\t+\r\n";
-	//stream.write(&data.as_bytes()).expect("Failed to send");
 
 	stream.flush().unwrap();
 	Ok(())
